b4c7316f447396b08d322914af2d5a185ae238da
jbeach3-elon <114613283+jbeach3-elon@users.noreply.github.com>
2022-11-25 17:37:05 +0000

    Update hook.h

--- hook.h
+++ hook.h
@@ -18,46 +18,7 @@ void generic_hook(PCONTEXT ctx)
 }
 */
 
-void tradeMaterial_hook(PCONTEXT ctx)
-{
-    printf("hooked tradeMaterial\n");
-
-    printf("a1: 0x%p\n", ctx->Rcx);
-    printf("a2: %s\n", ctx->Rdx);
-    printf("a3: 0x%p\n", ctx->R8);
-    printf("\n");
-}
-
 void InitializeHooks () 
 {
-
-    char game_version [30];
-    printf("enter Game version to load hooks from (elite-dangerous-odyssey-64 or elite-dangerous-64): ");
-    scanf("%s", game_version); printf("\n");
-
-    if(!strcmp(game_version, "elite-dangerous-odyssey-64"))
-    {
-        //set ody hooks here
-        printf("[*] loading hooks for: %s\n", game_version);
-
-        //set custom hook
-        HookInit("tradeMaterial\n", tradeMaterial_hook, true);
-        knownFunctionTable[GetIndexedFunction("tradeMaterial\n")].HookInfo.DisplayInfo.SetSilent();
-
-        //set a blank hook that just acts as a watchpoint
-        HookInit("collect_mat\n", nullptr, true);
-    
-    }
-
-    else if(!strcmp(game_version, "elite-dangerous-64"))
-    {
-        //set horizons hooks here
-        printf("[*] loading hooks for: %s\n", game_version);
-    }
-
-    else 
-    {
-        printf ("[!] unknown game version.. exiting\n");
-        abort();
-    }
+    //do the thing here
 }
