a47daf5214ff556d8dbd88c34c49501668ca1e9d
jbeach3-elon <114613283+jbeach3-elon@users.noreply.github.com>
2022-11-25 17:37:25 +0000

    Update hook.h

--- hook.h
+++ hook.h
@@ -3,7 +3,6 @@
 /*
 void generic_hook(PCONTEXT ctx)
 {
-
     //access args in this order
     DWORD64 * stackptr = reinterpret_cast<DWORD64 *>(ctx->Rsp);
     
@@ -13,7 +12,6 @@ void generic_hook(PCONTEXT ctx)
     ctx->R9 = 4;
     *(stackptr + 5) = 5;
     *(stackptr + 6) = 6;
-    
 
 }
 */
